894c636275ca0e7841bb1fcb684fba81f0fc4b13
Nikko Kowalow <2170644@lfschools.net>
2020-09-29 18:50:58 +0000

    finished reorder

--- Main.java
+++ Main.java
@@ -10,9 +10,28 @@ class Main {
      * See Readme.md
      * IMPORTANT: You may use only a single Stack as auxiliary storage. 
      */
+
     public static void reorder(Queue<Integer> q) 
     {
-        
+        Stack<Integer> stack = new Stack<Integer>();
+        while(!q.isEmpty()){
+          if(!stack.isEmpty()){
+            if(q.peek() < stack.peek()){
+              stack.push(q.remove());
+            } else {
+              while(!stack.isEmpty() && q.peek() > stack.peek()){
+                q.add(stack.pop());
+              }
+              stack.push(q.remove());
+            }
+          } else {
+            stack.push(q.remove());
+          }
+        }
+        while(!stack.isEmpty()){
+          q.add(stack.pop());
+        }
+
     }
 
 
